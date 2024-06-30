9a605a0cd513d96ebe26d7b00cfaba81ab2f1d8c
JJ Lee <maiload77@gmail.com>
2024-06-30 23:47:28 +0900

    Add: security 에 exceptionhandler 추가

--- src/main/kotlin/org/example/chatkopring/common/authority/SecurityConfig.kt
+++ src/main/kotlin/org/example/chatkopring/common/authority/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package org.example.chatkopring.common.authority
 
+import org.example.chatkopring.common.exception.CustomAccessDeniedHandler
+import org.example.chatkopring.common.exception.CustomAuthenticationEntryPoint
 import org.example.chatkopring.common.service.CustomOath2UserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -20,6 +22,8 @@ class SecurityConfig (
     private val jwtTokenProvider: JwtTokenProvider,
     private val customOath2UserService: CustomOath2UserService,
     private val oAuth2SuccessHandler: OAuth2SuccessHandler,
+    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,
+    private val customAccessDeniedHandler: CustomAccessDeniedHandler,
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -34,15 +38,17 @@ class SecurityConfig (
                     .requestMatchers("/api/member/**", "/api/chat/**").hasAnyRole("MEMBER", "OAUTH_MEMBER", "ADMIN")
                     .anyRequest().permitAll()
             }
+            .exceptionHandling {
+                it.authenticationEntryPoint(customAuthenticationEntryPoint)
+                it.accessDeniedHandler(customAccessDeniedHandler)
+            }
             .addFilter(corsFilter())
             .addFilterBefore(
                 JwtAuthenticationFilter(jwtTokenProvider),
                 UsernamePasswordAuthenticationFilter::class.java
             )
             .oauth2Login {
-                it
-//                    .loginPage("/login")
-                    .userInfoEndpoint { u -> u.userService(customOath2UserService) }
+                it.userInfoEndpoint { u -> u.userService(customOath2UserService) }
                     .successHandler(oAuth2SuccessHandler)
             }
 
---
+++ src/main/kotlin/org/example/chatkopring/common/exception/CustomAccessDeniedHandler.kt
@@ -0,0 +1,20 @@
+package org.example.chatkopring.common.exception
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAccessDeniedHandler: AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?
+    ) {
+        response?.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden")
+    }
+}
